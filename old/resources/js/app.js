78e2c96f0a8adafe0e8fb0d397c5955e190149c4
some <zubkoffalex4nder@yandex.ru>
2024-04-30 19:19:08 +0300

    ...

--- old/resources/js/app.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue';
-import App from '@/App.vue';
-import Router from '@/router/router.js';
-import Store from '@/store/store.js';
-
-createApp(App).use(Router).use(Store).mount('#app');
