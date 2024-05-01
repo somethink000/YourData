e50520a01129175ebf813afcf7ab7a8305d5e468
some <zubkoffalex4nder@yandex.ru>
2024-05-01 08:16:21 +0300

    vue base page

---
+++ resources/js/src/router/routes.js
@@ -0,0 +1,10 @@
+import About from '@/pages/About.vue';
+
+const routes = [
+	{
+		path: '/',
+		component: About,
+	},
+];
+
+export default routes;
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Auth::routes();
+//Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//Route::resource('data_containers', DataController::class);
+
+Route::get('{any?}', fn () => view('app'))->where('any', '.*');
